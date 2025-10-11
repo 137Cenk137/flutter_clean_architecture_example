c8fbf0bd3ec6c842faca0c4b67c8b7223218f392
Cenk Kıran <cenk33m@gmail.com>
2025-10-11 11:57:04 +0300

    Update dependencies for dotenv and flutter_dotenv, modify dependency injection to load environment variables, and include .env in .gitignore.

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,4 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppSecrets {
   static const String apiKey = 'YOUR_API_KEY';
   static const String apiSecret = 'YOUR_API_SECRET';
+  static String supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  static String supabaseAnonKey = dotenv.env['SUPABASE_API_KEY']!;
 }
--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -6,18 +6,25 @@ import 'package:flutter_clean_architecture/features/auth/presentation/bloc/auth_
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final serviceLocator = GetIt.instance;
 
 Future<void> initDependency() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+  await _addSupabaseDependency();
+  _addAuthDependency();
+}
+
+Future<void> _addSupabaseDependency() async {
   await Supabase.initialize(
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
   );
   //serviceLocator.registerFactory(); dotnet scope version of it
   serviceLocator.registerLazySingleton(() => Supabase.instance.client);
-  _addAuthDependency();
 }
 
 void _addAuthDependency() {
